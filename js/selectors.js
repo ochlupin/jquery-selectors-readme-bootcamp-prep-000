76f370e11a63cb89627f28d9de6bbd04e7ab1560
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-02 19:33:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,31 +1,15 @@
 function paragraphSelector(){
-<<<<<<< HEAD
   return $('p');
 }
 function lastImageSelector(){
-return $('img:last');
+  return $('img:last');
 }
 function ninjaBabySelector(){
-return $("#baby-ninja");
+  return $("#baby-ninja");
 }
 function divSelector(){
-return $('.pics');
+  return $('.pics');
 }
 function firstListItem(){
-return $('ul li:first-child');
-=======
-  $('p'):
-}
-function lastImageSelector(){
-
-}
-function ninjaBabySelector(){
-
-}
-function divSelector(){
-
-}
-function firstListItem(){
-  
->>>>>>> 530a370e252a391b71834b4be41ba95019359738
+  return $('ul li:first-child');
 }
